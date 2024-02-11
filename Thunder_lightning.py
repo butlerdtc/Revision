2cf807960cfba2f416359a56260cea34ed21cd80
butlerdtc <butlerr2@middleton.school.nz>
2024-02-11 20:18:46 +1300

    Update

---
+++ Thunder_lightning.py
@@ -0,0 +1,5 @@
+sound = 0.340
+time = float(input("How many seconds were there between the lightning and "
+                   "thunder: "))
+distance = sound * time
+print(f"The storm is {distance:.2f} kilometres away")
