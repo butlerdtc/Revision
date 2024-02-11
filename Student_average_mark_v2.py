905ad749e31b5f8aead55c2a82c14b5e334f302a
butlerdtc <butlerr2@middleton.school.nz>
2024-02-11 19:25:32 +1300

    Update

--- Student_average_mark_v2.py
+++ Student_average_mark_v2.py
@@ -32,3 +32,39 @@ if len(best_student) > 1:
     print(f"\twith {best_student[0][1]} marks")
 else:
     print(f"{best_student[0][0]} was the best with a mark of {best_mark}")
+
+for student_name in names:
+    if 100 >= student_name[1] >= 90:
+        print(f"{student_name[0]} scored an A+ with a mark of "
+              f"{student_name[1]}")
+    elif 90 > student_name[1] >= 85:
+        print(f"{student_name[0]} scored an A with a mark of "
+              f"{student_name[1]}")
+    elif 85 > student_name[1] >= 80:
+        print(f"{student_name[0]} scored an A- with a mark of "
+              f"{student_name[1]}")
+    elif 80 > student_name[1] >= 75:
+        print(f"{student_name[0]} scored a B+ with a mark of "
+              f"{student_name[1]}")
+    elif 75 > student_name[1] >= 70:
+        print(f"{student_name[0]} scored a B with a mark of "
+              f"{student_name[1]}")
+    elif 70 > student_name[1] >= 65:
+        print(f"{student_name[0]} scored a B- with a mark of "
+              f"{student_name[1]}")
+    elif 65 > student_name[1] >= 60:
+        print(f"{student_name[0]} scored a C+ with a mark of "
+              f"{student_name[1]}")
+    elif 60 > student_name[1] >= 55:
+        print(f"{student_name[0]} scored a C with a mark of "
+              f"{student_name[1]}")
+    elif 55 > student_name[1] >= 50:
+        print(f"{student_name[0]} scored a C- with a mark of "
+              f"{student_name[1]}")
+    elif 50 > student_name[1] >= 40:
+        print(f"{student_name[0]} scored a D with a mark of "
+              f"{student_name[1]}")
+    else:
+        print(f"{student_name[0]} scored an E with a mark of "
+              f"{student_name[1]}")
+        
